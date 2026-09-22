a42a5c9e094cdbd8ba346f49647ab4ba6b7b436c
Paul Agneray <82611035+Paul-Agn@users.noreply.github.com>
2026-09-22 16:40:18 +0200

    Update outils.js

--- outils.js
+++ outils.js
@@ -7,10 +7,11 @@ function pdfViewer(url, titre) {
 }
 
 /* Petite carte Google Maps d'un lieu (sans clé d'API) */
-function carteHtml(lieu) {
-  const q = encodeURIComponent(lieu);
+function carteHtml(lieu, coords) {
+  const q = encodeURIComponent(coords || lieu);
+  const nom = lieu;
   return '<div class="carte">' +
-    '<iframe src="https://www.google.com/maps?q=' + q + '&amp;output=embed&amp;hl=fr" title="Carte : ' + lieu + '" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
-    '<p class="lieu">' + lieu + ' · <a href="https://www.google.com/maps/search/?api=1&amp;query=' + q + '" target="_blank" rel="noopener">Ouvrir dans Google Maps</a></p>' +
+    '<iframe src="https://www.google.com/maps?q=' + q + '&amp;output=embed&amp;hl=fr" title="Carte : ' + nom + '" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+    '<p class="lieu">' + nom + ' · <a href="https://www.google.com/maps/search/?api=1&amp;query=' + q + '" target="_blank" rel="noopener">Ouvrir dans Google Maps</a></p>' +
     '</div>';
 }
